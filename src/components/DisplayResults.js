16aecf946debad97242a3b43cae6b9b817108cd2
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-04-11 00:24:06 +0800

    Development setup for news app

---
+++ src/components/DisplayResults.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayResults = () => {
+  return;
+};
+
+export default DisplayResults;
---
+++ src/components/MyFavPanel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyFavPanel = () => {
+  return;
+};
+
+export default MyFavPanel;
