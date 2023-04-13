1e2f9461723a2822501f125e8488d16120fb5c33
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-04-14 00:51:36 +0800

    style: roughly designed the item card and the grid in the display section

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Grid, Button } from "@mui/material";
+import { Grid, Box } from "@mui/material";
+import NewsItem from "./NewsItem";
 
 const DisplayResults = () => {
-  return;
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 4 }}>
+          {Array.from(Array(8)).map((_, index) => (
+            <Grid item xs={1} sm={1} md={1} key={index}>
+              <NewsItem></NewsItem>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default DisplayResults;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,7 +1,52 @@
 import React from "react";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  IconButton,
+  Typography,
+  Avatar,
+} from "@mui/material";
 
 const NewsItem = () => {
-  return;
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          // action={}
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="/static/images/cards/paella.jpg"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to cook together
+            with your guests. Add 1 cup of frozen peas along with the mussels, if you
+            like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </>
+  );
 };
 
 export default NewsItem;
