a4cc08d5f54fd84d61df0e703c75303ba7e3291f
Akmal Kamarudin <akmal.busy@gmail.com>
2023-04-14 23:10:08 +0800

    added .env to gitignore

--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL:
-    "https://newsapi.org/v2/everything?" +
-    "q=Apple&" +
-    "sortBy=publishedAt&" +
-    "apiKey=2a7530a76ae8496680afdfa64487b08b",
+require("dotenv").config();
+
+const instance = axios.create({
+  baseURL: "https://newsapi.org/v2/everything?", // Replace with your API base URL
+  headers: {
+    "X-Api-Key": process.env.REACT_APP_NEWS_API_KEY, // Use the name of your API key in the .env file
+  },
 });
+
+// Define a function that makes API request with a keyword parameter
+export const searchNews = (keyword) => {
+  return instance.get("everything", {
+    params: {
+      q: keyword, // Pass the keyword parameter in the API request
+    },
+  });
+};
+
+export default instance;
